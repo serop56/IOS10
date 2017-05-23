7d838bf45f0b55e44bfead4b075b95b082defe79
Serop Babikian <serop56@Serops-MacBook-Pro.local>
2017-05-23 14:23:20 +0400

    Initial Commit

---
+++ IOS10Dev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS10Dev
+//
+//  Created by Serop Babikian on 5/23/17.
+//  Copyright © 2017 Serop Babikian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
